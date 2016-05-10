f763e4ad610a01a97d3e27bc95baa479f6c458fd
Ryan-McBride <mcbrideryan89@gmail.com>
2016-05-10 13:27:43 -0700

    completed app

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// server.js
+(function(){"use strict";})();
+var express = require('express');
+var favicon = require('serve-favicon');
+var app = express();
+var path = require('path');
+
+
+var port = process.env.PORT || '3000';
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(path.normalize(__dirname + '/')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  console.log('reverse155 is listening at http://%s:%s -- %s', host, port);
+});
---
+++ src/public/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var questions = [];
+  var audio = new Audio('/public/ex.mp3');
+  $.ajax({
+    url: '/public/questions.json'
+  })
+  .done(function(data){
+    questions = data;
+  })
+
+  $('#button').on('click', function(){
+    audio.play();
+    var rand= Math.floor(Math.random()*questions.length);
+    $('#main').text(questions[rand].q);
+    $('#to').text(questions[rand].to);
+  });
+});
